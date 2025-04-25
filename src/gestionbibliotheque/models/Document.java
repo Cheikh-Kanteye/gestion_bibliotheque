27bb137dbe83076b6faeb387cd9b1f3b039a8c37
aminata-byte <aminatadiane175@gmail.com>
2025-04-25 23:36:05 +0000

    code complet des class

--- src/gestionbibliotheque/models/Document.java
+++ src/gestionbibliotheque/models/Document.java
@@ -1,26 +1,52 @@
-
 package gestionbibliotheque.models;
 
-public abstract class Document {
+import gestionbibliotheque.interfaces.Empruntable;
+
+abstract class Document implements Empruntable {
+
     protected int id;
     protected String titre;
     protected int anneePublication;
+    protected boolean disponible;
+    protected int emprunteurId;
 
-    public Document(int id, String titre, int anneePublication) {
-        this.id = id;
+    public Document(String titre, int anneePublication) {
         this.titre = titre;
         this.anneePublication = anneePublication;
+        this.disponible = true;
+        this.emprunteurId = -1;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
     }
 
     public abstract void afficherDetails();
+
+    @Override
+    public boolean emprunter(int utilisateurId) {
+        if (disponible) {
+            disponible = false;
+            emprunteurId = utilisateurId;
+
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public boolean retourner() {
+        if (!disponible) {
+            disponible = true;
+            emprunteurId = -1;
+            return true;
+        }
+        return false;
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
