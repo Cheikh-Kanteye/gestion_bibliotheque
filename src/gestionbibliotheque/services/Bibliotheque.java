317d867c30932e2d1f227c460a4e76d946ad02fc
cheikh-kanteye <cheikhkanteye.contact@gmail.com>
2025-04-25 22:26:33 +0000

    add bibliotheque

--- src/gestionbibliotheque/services/Bibliotheque.java
+++ src/gestionbibliotheque/services/Bibliotheque.java
@@ -4,10 +4,30 @@
  */
 package gestionbibliotheque.services;
 
+import gestionbibliotheque.models.User;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.text.Document;
+
 /**
  *
  * @author chika
  */
 public class Bibliotheque {
+    private List<Document> documents;
+    private List<User> users;
+    
+    
+    
+    public Bibliotheque(){
+        this.documents = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+    
+    
+    public void ajouterDocument(Document document){
+        documents.add(document);
+    }
+    
     
 }
